73e8d878e6104ac831486a0ccf2a802fa3eae146
Prajeesh Chavan <151425532+prajeesh-chavan@users.noreply.github.com>
2024-07-15 18:31:17 +0530

    Added Search Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
-import Navbar from './components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import Video from './Pages/Video/Video';
+import SearchResult from './Pages/SearchResult';
 
 function App() {
   const [sidebar, setSidebar] = useState(false);
+  const [query, setQuery] = useState("");
 
   return (
     <div>
-      <Navbar setSidebar={setSidebar} />
+      <Navbar setSidebar={setSidebar} onSearch={setQuery} />
       <Routes>
         <Route path='/' element={<Home sidebar={sidebar} />} />
         <Route path='/video/:categoryId/:videoId' element={<Video />} />
+        <Route path='/Search-Result' element={<SearchResult query={query} />} />
       </Routes>
     </div>
   );
